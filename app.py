b6ca47910b04dbe8e6b9007a34d37dd5a9a9548e
Yahya0001 <yahya.allaya@sumcom.tn>
2022-12-03 22:38:26 +0100

    cars recommandation

--- app.py
+++ app.py
@@ -1,35 +1,94 @@
-from flask import Flask, request , jsonify
+from flask import Flask, request , jsonify, render_template
+from flask_cors import CORS, cross_origin
+
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "hello yahya!"
+CORS(app)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 
-@app.route('/api/testobj', methods=['GET' , 'POST'])
-def testobj():
+
+data = pd.read_csv("./tayara_cars.csv")
+
+
+features =["location","km","marque","price","carburant","boite","cylindre"]
+
+for feature in features:
+  data[feature] = data[feature].fillna('')
+
+def combine_features(row):
+  try:
+    return row['location']+" "+str(row['km'])+" "+row['marque']+" "+str(row['price'])+" "+row['carburant']+" "+row['boite']+" "+row['cylindre']
+  except:
+     print("Error:  " + row) 
+     
+data["combine_features"] = data.apply(combine_features, axis=1)
+
+CV = CountVectorizer()
+count_matrix = CV.fit_transform(data["combine_features"])
+cosine_sim = cosine_similarity(count_matrix)
+
+def get_car_from_index(index):
+  return data[data.index == index]["combine_features"].values[0]
+
+
+def get_index_from_car(combine_features):
+  return data[data.combine_features == combine_features ]["Unnamed: 0"].values[0]
+
+
+
+@app.route('/', methods=['GET','POST'])
+@cross_origin()
+def index():
     request_type_str = request.method
     if request_type_str == 'GET' :
-        return jsonify(data = "http://127.0.0.1:5000/static/obj/f9af1d798bdec6ae00547baf9a8c72722f254cd15eeba67eb1480e8a05ff2e71.obj"), 200
+      return render_template('car.html',prediction={})
+
     if request_type_str == 'POST' :
+      location = request.form['location']
+      marque = request.form['marque']
+      cylindre = request.form['cylindre']
+      boite = request.form['boite']
+      carburant = request.form['carburant']
+      price = request.form['price']
+      km = request.form['km']
+      car_user_like = location+" "+ km +" " + marque +" "+ price +" "+ carburant +" "+ boite +" "+cylindre
 
-        objFile = request.files['objfile']
-        obj_path = "./static/obj/" + objFile.filename
-        objFile.save(obj_path)
 
+      # car_user_like ="Tunis, Autres villes 29000.0 Volkswagen 45000.0 Essence Nouveau Manuelle"
+      car_index = get_index_from_car(car_user_like)
+      similar_cars = list(enumerate(cosine_sim[car_index]))
+      sorted_similar_cars = sorted(similar_cars , key=lambda x:x[1], reverse=True)
 
-        return jsonify(data =obj_path), 201
+      d={}
+      i =0
+      for car in sorted_similar_cars :
+        x=get_car_from_index(car[0])
 
-@app.route('/api/3d_mobel' , methods=['GET' , 'POST'])
-def man():
-  request_type_str = request.method
-  if request_type_str == 'GET' :
-    return jsonify(data ="ttttttttt"), 200
-    #return render_template('index.html' , imagePath="static/imageModel.svg")
-  else :
-    inputData = request.get_json()
+        l= x.split()
+        car_d={}
+        car_d["cylindre"]=l.pop()
+        car_d["boite"]=l.pop()
+        car_d["carburant"]=l.pop()
+        car_d["price"]=l.pop()
+        car_d["marque"]=l.pop()
+        car_d["km"]=l.pop()
 
-    return jsonify( data = inputData['name']) , 200
+        ch = ""
+        for loc in l:
+          ch+= " " + loc
+        car_d["location"]=ch
+
+        d[i]= car_d
+        i += 1
+        if i> 9 :
+          break
+      
+      return render_template('car.html',prediction=d)
 
 
 
